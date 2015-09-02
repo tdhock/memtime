32a48972b67b5aa56a3f9277a83d9d8f3979d0c2
Toby Dylan Hocking <tdhock5@gmail.com>
2015-09-02 10:11:26 -0400

    first working version

--- R/free.profile.R
+++ R/free.profile.R
@@ -1,17 +1,26 @@
+free.profile.start <- function
 ### Start running free in the background.
-free.profile.start <- function(free.file){
+(free.file
+### File to which we will write output of free.
+ ){
   stopifnot(is.character(free.file))
   stopifnot(length(free.file) == 1)
-  sh.file <- system.file("exec", "free.sh", package="testthatQuantity")
+  sh.file <- system.file("exec", "free.sh", package="memtime")
   cmd <- paste("bash", sh.file, free.file)
   system(cmd, wait=FALSE)
+### Nothing.
 }
 
+free.profile.stop <- function
 ### Stop running free in the background.
-free.profile.stop <- function(free.file){
+(free.file
+### file where free output is writing.
+ ){
   stopifnot(is.character(free.file))
   stopifnot(length(free.file) == 1)
   DONE.file <- paste0(free.file, ".DONE")
   cat("", file=DONE.file)
+  readLines(free.file)
+### character vector of lines written to free.file.
 }
 
--- R/memtime.R
+++ R/memtime.R
@@ -1,10 +1,13 @@
-mem.time <- function
+memtime <- function
 ### Compute time and memory usage of R code.
 (expr,
 ### R code.
  gcFirst = TRUE,
 ### Run gc before computing initial time/memory values?
- free.file=tempfile()
+ free.file=tempfile(),
+### file where free output will be written.
+ sleep.seconds=0.1
+### Seconds to wait before/after calling free.profile.start/stop.
  ){
   ppt <- function(y) {
     if (!is.na(y[4L])) 
@@ -17,17 +20,32 @@ mem.time <- function
     return(rep(NA_real_, 5L))
   if (gcFirst) 
     gc(FALSE)
-  time <- proc.time()
   free.profile.start(free.file)
+  Sys.sleep(sleep.seconds)
+  time <- proc.time()
   on.exit(cat("Timing stopped at:", ppt(proc.time() - time), 
               "\n"))
   expr
   new.time <- proc.time()
-  free.profile.stop(free.file)
-  browser()
+  Sys.sleep(sleep.seconds)
+  free.lines <- free.profile.stop(free.file)
+  firstValue <- function(txt){
+    value <- sub(".*? ([0-9]+) .*", "\\1", txt)
+    as.numeric(value)
+  }
+  used.lines <- grep("-/+", free.lines, value=TRUE)
+  used.values <- firstValue(used.lines)
+  first <- used.values[1]
+  max.mem <- max(used.values)
+  megabytes <- 
+    c(first=first,
+      max=max.mem,
+      last=used.values[length(used.values)],
+      max.increase=max.mem-first,
+      total=firstValue(free.lines[2]))
   on.exit()
   list(time=structure(new.time - time, class = "proc_time"),
-       memory=NULL)
+       memory=data.frame(megabytes))
 ### List of time and memory usage.
 }
 
